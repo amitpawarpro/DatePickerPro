60304a7786b8c0e437e9e425873194236808f3fe
Amit <amity2001@gmail.com>
2024-05-02 02:37:05 +0530

    Minor cosmatic changes in StringUpDown

---
+++ DatePicker/ValueRolledEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatePickerPro
+{
+    public class ValueRolledEventArgs : EventArgs
+    {
+        public bool IsForword { get; set; }
+
+        public ValueRolledEventArgs(bool isForword)
+        {
+            IsForword = isForword;
+        }
+    }
+}
